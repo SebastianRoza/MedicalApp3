ba8b1bf181524f753711ee012641988528a0dff6
SebastianRoza <seba.rozanski@gmail.com>
2023-07-03 18:43:51 +0200

    MED-99

--- src/main/java/com/sebar/Medical/model/Doctor.java
+++ src/main/java/com/sebar/Medical/model/Doctor.java
@@ -1,6 +1,9 @@
 package com.sebar.Medical.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Getter
@@ -11,6 +14,9 @@ import lombok.*;
 @EqualsAndHashCode
 @Entity
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String specialization;
     private String email;
     private String firstName;
