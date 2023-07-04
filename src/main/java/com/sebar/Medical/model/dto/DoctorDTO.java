68c4c438a246e9e176dbb51df255c9cb7d7a948d
SebastianRoza <seba.rozanski@gmail.com>
2023-07-04 18:05:46 +0200

    MED-99

--- src/main/java/com/sebar/Medical/model/dto/DoctorDTO.java
+++ src/main/java/com/sebar/Medical/model/dto/DoctorDTO.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DoctorDTO {
+    private long id;
     private Specialization specialization;
     private String email;
     private String firstName;
